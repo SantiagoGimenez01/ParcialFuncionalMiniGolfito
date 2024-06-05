877cf8cef93612b5aee7c52c191ace8339b1e16d
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-06-05 13:57:02 -0300

    Modelo inicial de parcial

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,35 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+-- Modelo inicial
+data Jugador = UnJugador {
+  nombre :: String,
+  padre :: String,
+  habilidad :: Habilidad
+} deriving (Eq, Show)
+
+data Habilidad = Habilidad {
+  fuerzaJugador :: Number,
+  precisionJugador :: Number
+} deriving (Eq, Show)
+
+-- Jugadores de ejemplo
+bart = UnJugador "Bart" "Homero" (Habilidad 25 60)
+todd = UnJugador "Todd" "Ned" (Habilidad 15 80)
+rafa = UnJugador "Rafa" "Gorgory" (Habilidad 10 1)
+
+data Tiro = UnTiro {
+  velocidad :: Number,
+  precision :: Number,
+  altura :: Number
+} deriving (Eq, Show)
+
+type Puntos = Number
+
+-- Funciones útiles
+between n m x = elem x [n .. m]
+
+maximoSegun f = foldl1 (mayorSegun f)
+mayorSegun f a b
+  | f a > f b = a
+  | otherwise = b
