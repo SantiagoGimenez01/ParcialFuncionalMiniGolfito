3cb384134aa014a56e97e88ea1becf6a08452e8c
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-06-05 15:46:40 -0300

    Punto 1 hecho

--- src/Library.hs
+++ src/Library.hs
@@ -1,6 +1,9 @@
 module Library where
 import PdePreludat
 
+doble :: Number -> Number
+doble numero = numero + numero
+
 -- Modelo inicial
 data Jugador = UnJugador {
   nombre :: String,
@@ -33,3 +36,33 @@ maximoSegun f = foldl1 (mayorSegun f)
 mayorSegun f a b
   | f a > f b = a
   | otherwise = b
+
+--Punto 1
+type PaloDeGolf = Habilidad -> Tiro
+
+putter :: PaloDeGolf
+putter habilidad = UnTiro{velocidad = 10, precision = (doble . precisionJugador) habilidad , altura = 0}
+
+madera :: PaloDeGolf
+madera habilidad = UnTiro{velocidad = 100, precision = (mitad . precisionJugador) habilidad , altura = 5}
+
+mitad :: Number -> Number
+mitad numero = numero/2 
+
+hierros :: Number -> PaloDeGolf
+hierros n habilidad = 
+    UnTiro{velocidad = velociadadHierros n habilidad, precision = precisionHierros n habilidad , altura = alturaHierros n}
+
+velociadadHierros :: Number -> Habilidad -> Number
+velociadadHierros n habilidad = fuerzaJugador habilidad * n
+
+precisionHierros :: Number -> Habilidad -> Number
+precisionHierros n habilidad = precisionJugador habilidad / n
+
+alturaHierros :: Number -> Number
+alturaHierros 0 = 0
+alturaHierros 1 = 0
+alturaHierros 2 = 0
+alturaHierros n = n - 3
+
+data Palos = Putter | Madera | Hierros deriving(Show, Eq)
